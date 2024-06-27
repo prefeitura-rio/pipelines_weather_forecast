5833452c8b41cfcb11d6fabf7ab83e3bff851b99
patriciacatandi <patriciabcatandi@gmail.com>
2024-06-27 11:51:43 -0300

    changing .gitignore and removing logging file

--- pipelines/precipitation_model/rionowcast/gypscie/processors/utils/logging.py
+++ pipelines/precipitation_model/rionowcast/gypscie/processors/utils/logging.py
@@ -1,16 +1,23 @@
+# -*- coding: utf-8 -*-
+# pylint: disable= invalid-name, inconsistent-return-statements, broad-except
+"""
+ADD
+"""
+
 import logging
 
 
 class Logger(object):
-    
-    name = 'logger'
-    levels = {'debug': logging.DEBUG,
-              'info': logging.INFO,
-              'warning': logging.WARNING,
-              'error': logging.ERROR,
-              'critical': logging.CRITICAL}
-    
-    
+
+    name = "logger"
+    levels = {
+        "debug": logging.DEBUG,
+        "info": logging.INFO,
+        "warning": logging.WARNING,
+        "error": logging.ERROR,
+        "critical": logging.CRITICAL,
+    }
+
     @classmethod
     def add_log_parameters(cls, parser, root_basename):
         """Add predefined logging parameters to an argparse.ArgumentParser object
@@ -23,23 +30,25 @@ class Logger(object):
         Returns:
             argparse.ArgumentParser: the argparse.ArgumentParser object with logging parameters added
         """
-        logfile = (root_basename.split('.')[0]) + '.log'
-        parser.add_argument("--logfile",
-                            type = str,
-                            default = logfile,
-                            help = "Log filename. Default __file__ basename")
-        parser.add_argument("--loglevel",
-                            type = str,
-                            default = 'info',
-                            help = f"Log level. Level must be one of {list(Logger.levels.keys())}")
-        parser.add_argument("--verbose",
-                            action='store_true',
-                            help = "Enable verbose execution on stdout. Default is False.")
+        logfile = (root_basename.split(".")[0]) + ".log"
+        parser.add_argument(
+            "--logfile", type=str, default=logfile, help="Log filename. Default __file__ basename"
+        )
+        parser.add_argument(
+            "--loglevel",
+            type=str,
+            default="info",
+            help=f"Log level. Level must be one of {list(Logger.levels.keys())}",
+        )
+        parser.add_argument(
+            "--verbose",
+            action="store_true",
+            help="Enable verbose execution on stdout. Default is False.",
+        )
         return parser
 
-    
     @classmethod
-    def init(cls, filename, level='info', verbose=False):
+    def init(cls, filename, level="info", verbose=False):
         """Method that setup and start logging
 
         Args:
@@ -47,11 +56,11 @@ class Logger(object):
             level (str, optional): a python logging level. Defaults to 'info'.
             verbose (bool, optional): if True, output logging on stdout. Defaults to False.
         """
-        assert level in cls.levels.keys(), f'level must be one of {list(cls.levels.keys())}'
+        assert level in cls.levels.keys(), f"level must be one of {list(cls.levels.keys())}"
         logger = cls.get_logger()
         logger.setLevel(cls.levels[level])
-        formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-        fl = logging.FileHandler(f'logs/{filename}')
+        formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+        fl = logging.FileHandler(f"logs/{filename}")
         fl.setLevel(cls.levels[level])
         fl.setFormatter(formatter)
         logger.addHandler(fl)
@@ -60,8 +69,7 @@ class Logger(object):
             sh.setLevel(cls.levels[level])
             sh.setFormatter(formatter)
             logger.addHandler(sh)
-    
-    
+
     @classmethod
     def get_logger(cls):
         return logging.getLogger(cls.name)
