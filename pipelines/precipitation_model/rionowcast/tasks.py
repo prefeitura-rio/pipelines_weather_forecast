01c56a2778d5d0dd6525c0ddf9b2c7cd1a98c67a
patriciacatandi <patriciabcatandi@gmail.com>
2024-06-28 17:06:14 -0300

    fixing secret path for infisical

--- pipelines/precipitation_model/rionowcast/tasks.py
+++ pipelines/precipitation_model/rionowcast/tasks.py
@@ -16,8 +16,8 @@ from prefeitura_rio.pipelines_utils.logging import log
 from pipelines.constants import constants
 from pipelines.precipitation_model.rionowcast.utils import bq_project, GypscieApi
 
+
 # noqa E302, E303
-# pylint: disable=anomalous-backslash-in-string
 @task()
 def access_api():
     """# noqa E303
@@ -25,12 +25,12 @@ def access_api():
     """
     infisical_username = constants.INFISICAL_USERNAME.value
     infisical_password = constants.INFISICAL_PASSWORD.value
-    username = get_secret(infisical_username, path="\gypscie")
-    password = get_secret(infisical_password, path="\gypscie")
+    username = get_secret(infisical_username, path="/gypscie")
+    password = get_secret(infisical_password, path="/gypscie")
     log(f"[DEBUG] get_secret {username}")
     log(f"[DEBUG] get_secret {password}")
-    username = get_secret(infisical_username, path="\gypscie")[infisical_username]
-    password = get_secret(infisical_password, path="\gypscie")[infisical_password]
+    username = get_secret(infisical_username, path="/gypscie")[infisical_username]
+    password = get_secret(infisical_password, path="/gypscie")[infisical_password]
     log("\n\n[DEBUG]: username from infisical: {username} {type(username)} ")
     log("\n\n[DEBUG]: password from infisical: {password}")
     # info = json.loads(base64.b64decode(secret))
